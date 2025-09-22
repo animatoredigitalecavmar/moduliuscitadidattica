31966f78620162f3c6a712ec02ee3b012f9d097d
animatoredigitalecavmar <animatoredigitale@cavalierimarignoni.it>
2025-09-22 16:00:55 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,13 @@
-const CACHE_NAME = 'moduli-cache-v1';
-const urlsToCache = [
-  '/moduliuscitadidattica/',
-  '/moduliuscitadidattica/index.html',
-  '/moduliuscitadidattica/manifest.json',
-  '/moduliuscitadidattica/modulo-autorizzazione-organizzazione.pdf',
-  '/moduliuscitadidattica/modulo-autorizzazione-genitori.pdf',
-  '/moduliuscitadidattica/modulo-pagamento.pdf',
-  '/moduliuscitadidattica/icon-192.png',
-  '/moduliuscitadidattica/icon-512.png'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log('Service Worker installato.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker attivato.');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+  // Qui puoi aggiungere caching in futuro
+  console.log('Richiesta intercettata:', event.request.url);
 });
